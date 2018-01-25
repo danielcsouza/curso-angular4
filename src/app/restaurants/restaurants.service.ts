8b5300fccb88fc3f32f61e437788d2ec72c3f949
Daniel Souza <daniel.carlos.souza@gmail.com>
2018-01-25 14:23:34 -0200

    tratamento de erros catch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,20 +1,19 @@
-
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model';
 import { MEAT_API } from '../app.api';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-
-
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { ErrorHandler } from 'app/app.error-handler';
 
 @Injectable()
 export class RestaurantsServices{
       constructor(private http:Http) {}
-
       restaurants():Observable<Restaurant[]>{
           return this.http.get(MEAT_API + '/restaurants')
-          .map(r => r.json());
+          .map(r => r.json())
+          .catch(ErrorHandler.handleError)
       }
-
 }
