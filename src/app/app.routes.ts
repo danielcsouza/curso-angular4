b66724a5230cc945123ccd17f4bc035b90281483
Daniel Souza <daniel.carlos.souza@gmail.com>
2018-01-25 16:14:06 -0200

    details restaurant inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from 'app/about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent},
     {path: 'restaurants', component: RestaurantsComponent}
 // tslint:disable-next-line:eofline
 ];
